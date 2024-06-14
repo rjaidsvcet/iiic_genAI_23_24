c421b3087cb049356d9b06c739453d2a5b748b92
rjaidsvcet <raunak.joshi@vcet.edu.in>
2024-06-14 12:37:35 +0530

    REST API Concept Flask

---
+++ 2024_06_14/5_rest/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask (__name__)
+cors = CORS (app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route ('/', methods = ['POST'])
+@cross_origin()
+def genericFunction ():
+    firstname = request.json['firstname']
+    print (firstname)
+    return jsonify ({
+        'output' : firstname
+    })
+
+if __name__ == '__main__':
+    app.run (debug=True)
