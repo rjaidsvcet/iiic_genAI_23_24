885dff2c23f94414c8c21d11ae4a8172601fe9d6
rjaidsvcet <raunak.joshi@vcet.edu.in>
2024-06-19 11:26:49 +0530

    Streamlit for LLM

--- 2024_06_19/1/main.py
+++ 2024_06_19/1/main.py
@@ -1,6 +1,10 @@
 from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
 # from langchain.chains import LLMChain
+import streamlit as st
+
+st.title ('Celebrity Information using Large Language Model')
+inputText = st.text_input ('Search the celebrity you want')
 
 inputPrompt = PromptTemplate (
     input_variables = ['name'],
@@ -17,5 +21,10 @@ llm = Ollama (model = 'llama3')
 
 chain = inputPrompt | llm
 
-response = chain.invoke ({'name' : 'Marlon Brando'})
-print (response)
+# response = chain.invoke ({'name' : 'Marlon Brando'})
+# print (response)
+
+if inputText:
+    st.write (chain.invoke ({
+        inputText
+    }))
